3e6e6f2210018ee9d7b80731883cdbced166df4c
GuMiner <gus.gran@outlook.com>
2015-12-06 20:58:31 -0800

    Writing the generation code for the sun and asteroids, pending the spherical generation code itself

--- Geometry.cpp
+++ Geometry.cpp
@@ -3,53 +3,47 @@
 #include "Geometry.h"
 
 // Generates the trianges for a spherical shape with the specified major axis deformation, per-point deformation, and radius.
-colorBarycentricVertex* Geometry::GenerateSphericalArchetype(float radius, float majorAxisDeformation, float perPointDeformation)
+colorBarycentricVertex* Geometry::GenerateSphericalArchetype(float radius, float majorAxisDeformation, float perPointDeformation, float triangleSize)
 {
     return nullptr;
 }
 
-// TODO all values should be configuration-modifiable
-
+// Generates the sun, which is large.
 colorBarycentricVertex* Geometry::GenerateSun()
 {
-    return nullptr;
+    return GenerateSphericalArchetype(
+        ConfigManager::SunSize,
+        0.0f,
+        Constants::Rand(ConfigManager::SunMaxPerPointDeformation),
+        ConfigManager::SunTriangleSize);
 }
 
 // Generates a small asteroid, which means it is less than a grid size (~0.1 units large)
 colorBarycentricVertex* Geometry::GenerateSmallAsteroid()
 {
-    float size = 0.1f;
-    float sizeVariation = 0.05f;
-    float maxDeformation = 0.05f;
-    float maxPerPointDeformation = 0.02f;
-
-    float majorAxisDeformation = Constants::Rand(maxDeformation);
-    float perPointDeformation = Constants::Rand(maxPerPointDeformation);
-    return GenerateSphericalArchetype(size + Constants::Rand(sizeVariation), majorAxisDeformation, perPointDeformation);
+    return GenerateSphericalArchetype(
+        ConfigManager::SmallAsteroidSize + Constants::Rand(ConfigManager::SmallAsteroidSizeMaxVariation),
+        Constants::Rand(ConfigManager::SmallAsteroidSizeMaxAxisDeformation),
+        Constants::Rand(ConfigManager::SmallAsteroidSizeMaxPerPointDeformation),
+        ConfigManager::SmallAsteroidTriangleSize);
 }
 
 // Generates a medium asteroid, which means it is about 40% of a grid size.
 colorBarycentricVertex* Geometry::GenerateMediumAsteroid()
 {
-    float size = 0.4f;
-    float sizeVariation = 0.2f;
-    float maxDeformation = 0.1f;
-    float maxPerPointDeformation = 0.05f;
-
-    float majorAxisDeformation = Constants::Rand(maxDeformation);
-    float perPointDeformation = Constants::Rand(maxPerPointDeformation);
-    return GenerateSphericalArchetype(size + Constants::Rand(sizeVariation), majorAxisDeformation, perPointDeformation);
+    return GenerateSphericalArchetype(
+        ConfigManager::MediumAsteroidSize + Constants::Rand(ConfigManager::MediumAsteroidSizeMaxVariation),
+        Constants::Rand(ConfigManager::MediumAsteroidSizeMaxAxisDeformation),
+        Constants::Rand(ConfigManager::MediumAsteroidSizeMaxPerPointDeformation),
+        ConfigManager::MediumAsteroidTriangleSize);
 }
 
 // Generates a large asteroid, which means it is about the size of the grid.
 colorBarycentricVertex* Geometry::GenerateLargeAsteroid()
 {
-    float size = 1.0f;
-    float sizeVariation = 0.2f;
-    float maxDeformation = 0.2f;
-    float maxPerPointDeformation = 0.1f;
-
-    float majorAxisDeformation = Constants::Rand(maxDeformation);
-    float perPointDeformation = Constants::Rand(maxPerPointDeformation);
-    return GenerateSphericalArchetype(size + Constants::Rand(sizeVariation), majorAxisDeformation, perPointDeformation);
+    return GenerateSphericalArchetype(
+        ConfigManager::LargeAsteroidSize + Constants::Rand(ConfigManager::LargeAsteroidSizeMaxVariation),
+        Constants::Rand(ConfigManager::LargeAsteroidSizeMaxAxisDeformation),
+        Constants::Rand(ConfigManager::LargeAsteroidSizeMaxPerPointDeformation),
+        ConfigManager::LargeAsteroidTriangleSize);
 }
--- Geometry.h
+++ Geometry.h
@@ -4,7 +4,7 @@
 // Generates in-game geometry so that asteroids are *always* different per game but *always* consistent once generated.
 class Geometry
 {
-    colorBarycentricVertex* GenerateSphericalArchetype(float radius, float majorAxisDeformation, float perPointDeformation);
+    colorBarycentricVertex* GenerateSphericalArchetype(float radius, float majorAxisDeformation, float perPointDeformation, float triangleSize);
 public:
     colorBarycentricVertex* GenerateSun();
     colorBarycentricVertex* GenerateSmallAsteroid();
